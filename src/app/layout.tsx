2cabe0817ecb8d31ca27615d8674a81792c86b74
John Ashly <johnshop2306@gmail.com>
2025-06-10 15:25:31 +0000

    Error: In HTML, whitespace text nodes cannot be a child of <head>. Make

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,18 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta name="application-name" content="LeyText Learn" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="LeyText Learn" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-config" content="/icons/browserconfig.xml" />
-        <meta name="msapplication-TileColor" content="#3B82F6" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#3B82F6" />
-      </head>
+      <head><meta name="application-name" content="LeyText Learn" /><meta name="apple-mobile-web-app-capable" content="yes" /><meta name="apple-mobile-web-app-status-bar-style" content="default" /><meta name="apple-mobile-web-app-title" content="LeyText Learn" /><meta name="format-detection" content="telephone=no" /><meta name="mobile-web-app-capable" content="yes" /><meta name="msapplication-config" content="/icons/browserconfig.xml" /><meta name="msapplication-TileColor" content="#3B82F6" /><meta name="msapplication-tap-highlight" content="no" /><meta name="theme-color" content="#3B82F6" /></head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
